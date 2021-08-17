267e1cff8862387cd4f283f2dab1b4a5e6933f5d
abeni16 <abeni3ase7@gmail.com>
2021-08-17 13:30:54 +0300

    single page

--- src/componets/MainComponent.js
+++ src/componets/MainComponent.js
@@ -3,8 +3,12 @@ import DishDetail from "./DishdetailComponent";
 import Menu from "./MenuComponet";
 import Footer from "./FooterComponents";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import Header from "./HeaderComponent";
 import Home from "./HomeComponents";
+import Contact from "./ContactComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
     this.state = {
       dishes: DISHES,
       selectedDish: null,
+      Comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
   onDishselect(dishId) {
@@ -20,7 +27,13 @@ class Main extends Component {
   }
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
@@ -32,6 +45,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -2,149 +2,45 @@ export const DISHES = [
   {
     id: 0,
     name: "Uthappizza",
-    image: "assets/images/uthappizza.png",
+    image: "/assets/images/uthappizza.png",
     category: "mains",
     label: "Hot",
     price: "4.99",
+    featured: true,
     description:
-      "A unique combination of Indian Uthappam (pancake) and Italian pizzachillies and Buffalo Paneer.",
-    comments: [
-      {
-        id: 0,
-        comment: "Imagine all the eaables, living in conFusion!",
-        author: "John lemon",
-        date: "Oct/17/2012",
-      },
-      {
-        id: 1,
-        comment:
-          "Sends anyone to heavenm I could get my mother-in law to eat it!",
-        author: "Paul McVites",
-        date: "Sep/06/2014",
-      },
-      {
-        id: 2,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-      {
-        id: 3,
-        comment: "this so delcious food i love it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-    ],
+      "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
   },
   {
     id: 1,
     name: "Zucchipakoda",
-    image: "assets/images/zucchipakoda.png",
+    image: "/assets/images/zucchipakoda.png",
     category: "appetizer",
     label: "",
     price: "1.99",
+    featured: false,
     description:
       "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-    comments: [
-      {
-        id: 0,
-        comment: "Imagine all the eaables, living in conFusion!",
-        author: "John lemon",
-        date: "Oct/17/2012",
-      },
-      {
-        id: 1,
-        comment:
-          "Sends anyone to heavenm I could get my mother-in law to eat it!",
-        author: "Paul McVites",
-        date: "Sep/06/2014",
-      },
-      {
-        id: 2,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-      {
-        id: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-    ],
   },
   {
     id: 2,
     name: "Vadonut",
-    image: "assets/images/vadonut.png",
+    image: "/assets/images/vadonut.png",
     category: "appetizer",
     label: "New",
     price: "1.99",
+    featured: false,
     description:
       "A quintessential ConFusion experience, is it a vada or is it a donut?",
-    comments: [
-      {
-        id: 0,
-        comment: "Imagine all the eaables, living in conFusion!",
-        author: "John lemon",
-        date: "Oct/17/2012",
-      },
-      {
-        id: 1,
-        comment:
-          "Sends anyone to heavenm I could get my mother-in law to eat it!",
-        author: "Paul McVites",
-        date: "Sep/06/2014",
-      },
-      {
-        id: 2,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-      {
-        id: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-    ],
   },
   {
     id: 3,
     name: "ElaiCheese Cake",
-    image: "assets/images/elaicheesecake.png",
+    image: "/assets/images/elaicheesecake.png",
     category: "dessert",
     label: "",
     price: "2.99",
+    featured: false,
     description:
       "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-    comments: [
-      {
-        id: 0,
-        comment: "Imagine all the eaables, living in conFusion!",
-        author: "John lemon",
-        date: "Oct/17/2012",
-      },
-      {
-        id: 1,
-        comment:
-          "Sends anyone to heavenm I could get my mother-in law to eat it!",
-        author: "Paul McVites",
-        date: "Sep/06/2014",
-      },
-      {
-        id: 2,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-      {
-        id: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "Feb/14/2015",
-      },
-    ],
   },
 ];
