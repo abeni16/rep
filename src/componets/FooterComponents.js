b7d049a4e4356e359f9d9c2ffd1faf85819b9ad8
Abel Argaw <abelargaw1993@gmail.com>
2022-02-08 04:48:49 -0800

    footer component change

--- src/componets/FooterComponents.js
+++ src/componets/FooterComponents.js
@@ -31,7 +31,7 @@ function Footer(props) {
               <br />
               HONG KONG
               <br />
-              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678
+              <i className="fa fa-phone fa-lg"></i>: +433 1234 5678
               <br />
               <i className="fa fa-fax fa-lg"></i>: +852 8765 4321
               <br />
