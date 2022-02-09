fe404d1d81e198ac75fda4427922e3d818ca0a91
Abel Legesse <abellegesse151@gmail.com>
2022-02-09 14:51:09 +0300

    modified some text inside AboutComponent.js

--- src/componets/AboutComponent.js
+++ src/componets/AboutComponent.js
@@ -49,8 +49,8 @@ function About(props) {
         <div className="col-12 col-md-6">
           <h2>Our History</h2>
           <p>
-            Started in 2010, Ristorante con Fusion quickly established itself as
-            a culinary icon par excellence in Hong Kong. With its unique brand
+            Started in 2014, Ristorante con Fusion quickly established itself as
+            a culinary icon par excellence in Addis Ababa. With its unique brand
             of world fusion cuisine that can be found nowhere else, it enjoys
             patronage from the A-list clientele in Hong Kong. Featuring four of
             the best three-star Michelin chefs in the world, you never know what
@@ -58,7 +58,7 @@ function About(props) {
           </p>
           <p>
             The restaurant traces its humble beginnings to{" "}
-            <em>The Frying Pan</em>, a successful chain started by our CEO, Mr.
+            <b>The Frying Pan</b>, a successful chain started by our CEO, Mr.
             Peter Pan, that featured for the first time the world's best
             cuisines in a pan.
           </p>
